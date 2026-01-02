328aa5c454d48824783906e92fd1569b728943d6
Jeon Yo'eun <sev314@gmail.com>
2026-01-02 12:31:46 +0900

    ♻️ `BackupMan` 클래스 뽑아내기

--- shingshingBP/NCBP.py
+++ shingshingBP/NCBP.py
@@ -686,41 +686,52 @@ Public License instead of this License.  But first, please read
 
 """
 
+import os
+import subprocess
+import time
+
+import selenium
+from selenium import webdriver
+
+SeleniumWebDriver = webdriver.Firefox | webdriver.Edge | webdriver.Chrome
+
+
+class BackupMan:
+    username: str | None
+    driver: SeleniumWebDriver
+
+    def init(self) -> None:
+        ##-------프로그램 초기화--------
+        print("프로그램 초기화중....")
+
+        try:
+            self.username = os.environ.get("USERNAME")
+
+            self.driver = webdriver.Edge()
+            self.driver.implicitly_wait(1)
+            self.driver.get("https://naver.com")
+
+            if os.path.exists("C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"):
+                print("wkHTMLtoPDF가 설치되어 있습니다. 프로그램이 준비되었습니다")
+            else:
+                print("wkHTMLtoPDF가 없습니다. 준비되지 않았습니다. wkHTMLtoPDF를 재설치 하세요")
+                print("계속할수는 있지만 정상적으로 진행되지 않습니다.")
+
+        except:
+            print("프로그램 초기화에 실패했습니다")
+            print("Selenium 설치여부와 위치를 확인해주세요")
+
+        ##--------초기화 끝----------------
+
+
 print("크롤링 프로그램 시작중...")
 print("사용중 문제나 어려움이 있을시, 스크린샷 첨부해서 admin@nonaver.com으로 메일 주시면 도와드리겠습니다.")
 print("NCBP 0.39버젼입니다. 2022년 02월 08일 배포.")
 
 # -----------------------아래부터 프로그램 시작-----------------------------
-##-------프로그램 초기화,모듈 로딩--------
-print("프로그램 초기화중....")
 
-try:
-    import os
-
-    import selenium
-
-    username = os.environ.get("USERNAME")
-
-    import subprocess
-    import time
-
-    from selenium import webdriver
-
-except:
-    print("프로그램 초기화에 실패했습니다")
-    print("Selenium 설치여부와 위치를 확인해주세요")
-
-driver = webdriver.Edge()
-driver.implicitly_wait(1)
-driver.get("https://naver.com")
-
-if os.path.exists("C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"):
-    print("wkHTMLtoPDF가 설치되어 있습니다. 프로그램이 준비되었습니다")
-else:
-    print("wkHTMLtoPDF가 없습니다. 준비되지 않았습니다. wkHTMLtoPDF를 재설치 하세요")
-    print("계속할수는 있지만 정상적으로 진행되지 않습니다.")
-
-##--------초기화,모듈로딩 끝----------------
+backup_man = BackupMan()
+backup_man.init()
 
 ##-------크롤링 사이트 로그인요청/사이트 지정----------------
 
@@ -741,7 +752,7 @@ print("카페 설정이 완료되었습니다.")
 
 sleeptime = input("딜레이를 몇 초나 줄지 입력하세요: ")
 
-print("알림:사용자이름은 %s 입니다" % username)
+print("알림:사용자이름은 %s 입니다" % backup_man.username)
 print("컴퓨터 설정이 완료되었습니다.")
 
 # 본격 크롤링 시작.
@@ -751,11 +762,11 @@ tno = start
 while tno <= end:
     no = cafedir + "/" + str(tno)
 
-    driver.get(no)
+    backup_man.driver.get(no)
     time.sleep(int(sleeptime))
 
     try:
-        alert = driver.switch_to.alert
+        alert = backup_man.driver.switch_to.alert
 
         alert.accept()
         print("%d번 게시글은 존재하지 않음" % tno)
@@ -763,13 +774,13 @@ while tno <= end:
         tno = tno + 1
 
     except:
-        driver.switch_to.frame("cafe_main")
+        backup_man.driver.switch_to.frame("cafe_main")
 
-        html = driver.page_source.encode("utf-8")
+        html = backup_man.driver.page_source.encode("utf-8")
         html = html.decode("utf-8")
 
         f = open(
-            "C:/Users/%s/NCBP/CAFE/%d.html" % (username, int(tno)),
+            "C:/Users/%s/NCBP/CAFE/%d.html" % (backup_man.username, int(tno)),
             "w",
             encoding="UTF-8",
         )
@@ -791,14 +802,14 @@ while tno <= end:
         print("%d번 게시글 저장완료." % int(tno))
         os.system(
             'start cmd /c start /d "C:/Program Files/wkhtmltopdf/bin/" /b wkhtmltopdf.exe --encoding UTF-8 C:/Users/%s/NCBP/CAFE/%d.html C:/Users/%s/NCBP/CAFE/%d.pdf'
-            % (username, tno, username, tno)
+            % (backup_man.username, tno, backup_man.username, tno)
         )
         print("%d번 게시글 변환요청 완료." % int(tno))
 
         tno = tno + 1
 
 print("크롤링이 완료되었습니다")
-os.system("start C:/Users/%s/NCBP/CAFE" % (username))
+os.system("start C:/Users/%s/NCBP/CAFE" % (backup_man.username))
 
 print("크롤링 결과를 확인하세요. %d번 게시글 부터 %d번 게시글까지 크롤링되었습니다." % (start, end))
 print("존재하지 않는 게시글은 저장되지 않았습니다.")

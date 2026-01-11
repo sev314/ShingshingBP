1233bc7fad512ea210f52d081a1f96307c49d9d4
Jeon Yo'eun <sev314@gmail.com>
2026-01-11 21:08:03 +0900

    🏗️ 모듈 구조 개선

--- installer/NCBP.bat
+++ installer/NCBP.bat
@@ -1,6 +1,6 @@
 ECHO OFF
 ECHO Ω««‡¡ﬂ...
 
-uv run python -m idlelib -r C:\Users\%UserName%\NCBP\programdata\NCBP.py
+uv run python -m idlelib -r C:\Users\%UserName%\NCBP\programdata\main.py
 
 exit
